185c7c9d3191d503233dbd5f4d8edfeb2e610313
David Rojo <david.rojo1@udea.edu.co>
2021-11-15 19:32:13 -0500

    Configuración empresa

---
+++ cofigurar_empresa/js/index.js
@@ -0,0 +1,103 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	nit: /^\d{7,14}$/, // 7 a 14 numeros.
+    direccion: /^[a-zA-ZÀ-ÿ\s0-9\-\#]{1,40}$/, // Letras y espacios, pueden llevar guion y numeral
+	actividad: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	trabajadores: /^\d{1,20}$/, // 10 a 20 numeros.
+	juridica: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	tipo: /^[a-zA-ZÀ-ÿ\s]{1,40}$/ // Letras y espacios, pueden llevar acentos.
+}
+
+const campos = {
+	nombre: false,
+	nit: false,
+    direccion: false,
+	actividad: false,
+	trabajadores: false,
+	juridica: false,
+	telefono: false,
+	correo: false,
+	tipo: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "nit":
+			validarCampo(expresiones.nit, e.target, 'nit');
+		break;
+		case "direccion":
+			validarCampo(expresiones.direccion, e.target, 'direccion');
+		break;
+		case "actividad":
+			validarCampo(expresiones.actividad, e.target, 'actividad');
+		break;
+		case "riesgo":
+			validarCampo(expresiones.riesgo, e.target, 'riesgo');
+		break;
+		case "trabajadores":
+			validarCampo(expresiones.trabajadores, e.target, 'trabajadores');
+		break;
+		case "juridica":
+			validarCampo(expresiones.juridica, e.target, 'juridica');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "tipo":
+			validarCampo(expresiones.tipo, e.target, 'tipo');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	if(campos.nombre && campos.nit && campos.direccion && campos.actividad && campos.trabajadores && campos.juridica && campos.telefono && campos.correo && campos.tipo){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
